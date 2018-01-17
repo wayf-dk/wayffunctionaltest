2993a032d49f6fbb4e26e9440f30de2a740f887d
Mads Freek <freek@wayf.dk>
2018-01-17 15:17:31 +0100

    working on tests for the hub sending signatures based on sha256

--- wayffunctionaltest_test.go
+++ wayffunctionaltest_test.go
@@ -780,24 +780,17 @@ func TestDigestMethodSha256_1(t *testing.T) {
 	// We need to get at the wayf:wayf elements - thus we got directly to the feed !!!
 	//	spmd := newMD("https://phph.wayf.dk/raw?type=feed&fed=wayf-fed")
 	expected := ""
-	//entityID := testSPs.Query1(nil, "/*/*/md:SPSSODescriptor/md:NameIDFormat[.='urn:oasis:names:tc:SAML:2.0:nameid-format:persistent']/../md:AttributeConsumingService/md:RequestedAttribute[@Name='urn:oid:1.3.6.1.4.1.5923.1.1.1.10' or @Name='eduPersonTargetedID']/../../../@entityID")
-	entityID := testSPs.Query1(nil, "/*/*/md:Extensions/wayf:wayf/wayf:SigningMethod/../../../@entityID")
-	entitymd, _ := Md.Internal.MDQ(entityID)
-	if entitymd == nil {
-		log.Fatalln("no SP found for testing Digest Method256")
-	}
+	entitymd, _ := Md.Internal.MDQ("https://wayfsp.wayf.dk")
 
 	tp := browse(nil, &overwrites{"Spmd": entitymd})
 	if tp != nil {
 		samlresponse, _ := gosaml.Html2SAMLResponse(tp.Responsebody)
-		entityID := entitymd.Query1(nil, "@entityID")
-		/*nameidformat := samlresponse.Query1(nil, "//saml:NameID/@Format")
-		nameid := samlresponse.Query1(nil, "//saml:NameID")
-		audience := samlresponse.Query1(nil, "//saml:Audience")
-		spnamequalifier := samlresponse.Query1(nil, "//saml:NameID/@SPNameQualifier")
-		eptid := samlresponse.Query1(nil, "//saml:Attribute[@Name='urn:oid:1.3.6.1.4.1.5923.1.1.1.10' or @Name='eduPersonTargetedID']/saml:AttributeValue")
-		fmt.Printf("%s %s %s %s %s\n", nameidformat, nameid, eptid, audience, spnamequalifier)
-		expected += `urn:oasis:names:tc:SAML:2.0:nameid-format:persistent {{.pnameid}} {{.eptid}} ` + entityID + ` ` + entityID + "\n"*/
+		signatureMethod := samlresponse.Query1(nil, "//ds:SignatureMethod/@Algorithm")
+		digestMethod := samlresponse.Query1(nil, "//ds:DigestMethod/@Algorithm")
+		fmt.Printf("%s\n%s\n", signatureMethod, digestMethod)
+		expected += `http://www.w3.org/2001/04/xmldsig-more#rsa-sha256
+http://www.w3.org/2001/04/xmlenc#sha256
+`
 	}
 	stdoutend(t, expected)
 }
